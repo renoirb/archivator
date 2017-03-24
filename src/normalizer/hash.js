fa3ebfc286d0f331c98c76463307ae4e4a007d56
Renoir Boulanger <hello@renoirboulanger.com>
2017-03-24 16:30:24 -0400

    New normalizer, make a hash of an URL return a filename and extension

---
+++ src/normalizer/hash.js
@@ -0,0 +1,52 @@
+'use strict';
+
+import crypto from 'crypto';
+import {URL} from 'url';
+
+/**
+ * Rewrite a file name based on an URL they were downloaded from
+ */
+
+export default resourceUrl => {
+  /**
+   * const hashes = crypto.getHashes();
+   * Yields available hashes
+   * ['DSA', 'DSA-SHA', 'DSA-SHA1', ...]
+   *
+   * See also list of message digests with locally installed OpenSSL/LibreSSL
+   * #TODO
+   *
+   * SHA1 should be enough, we do not want a too long file name.
+   * Ideally it would be something very short but with high entropy
+   * with elliptic curve type of hashes.
+   * Or maybe we could just truncate the string.
+   * Food for thoughts. #TODO
+   */
+  const hash = crypto.createHash('sha1');
+  let urlObj = {};
+  try {
+    urlObj = new URL(resourceUrl);
+  } catch (err) {
+    throw new Error(resourceUrl, err);
+  }
+
+  const pathname = urlObj.pathname;
+  // svg, png, jpg, webm
+  let extension = pathname.match(/(\.[a-z]{2,})$/i);
+  if (extension === null) {
+    /**
+     * We want an extension, but maybe we should support loading the resource via HTTP
+     * and grab mime-type and figure out what extension.
+     * Maybe we could have an optional parameter to this function and if it exists
+     * and is a string, starting by a dot, we just append that.
+     * Food for thoughts. #TODO
+     */
+    extension = '.img';
+  } else {
+    extension = extension[0];
+  }
+  // console.log(resourceUrl, extension); // DEBUG
+  const rv = hash.update(resourceUrl).digest('hex') + extension.toLowerCase();
+  // console.log(rv); // DEBUG
+  return rv;
+};
--- test/normalizer.js
+++ test/normalizer.js
@@ -2,6 +2,8 @@
 
 import slugs from '../src/normalizer/slugs';
 import assets from '../src/normalizer/assets';
+import hash from '../src/normalizer/hash';
+
 import {inputExpectedOutputStringsTests} from './utils';
 
 function assetsArgumentStringSplitter(input) {
@@ -9,10 +11,11 @@ function assetsArgumentStringSplitter(input) {
   return assets.apply(null, args);
 }
 
-const testSubjects = {slugs, assets: assetsArgumentStringSplitter};
+const testSubjects = {slugs, assets: assetsArgumentStringSplitter, hash};
 
 const testCases = {};
 testCases.slugs = require('./normalizer/slugs.csv');
 testCases.assets = require('./normalizer/assets.csv');
+testCases.hash = require('./normalizer/hash.csv');
 
 inputExpectedOutputStringsTests(testCases, testSubjects);
