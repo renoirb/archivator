1029cc87f48303411246affbbc922ae6c269d835
Renoir Boulanger <contribs@renoirboulanger.com>
2020-12-18 16:31:12 -0300

    chore: url-dirname-normalizer more tests, support removing accents

--- packages/url-dirname-normalizer/src/main.ts
+++ packages/url-dirname-normalizer/src/main.ts
@@ -1,19 +1,27 @@
 import { searchParams, pathName, toUrl } from './normalizer'
 
 /**
- * An URL to filesystem path normalizer.
+ * dirnameNormalizer — An URL to filesystem path normalizer.
  *
  * @public
  *
  * For any URL, we want a valid filesystem path
  * in which we might want to archive files to.
  *
- * {@link https://github.com/renoirb/archivator/blob/v2.0.0/src/normalizer/slugs.js#L18-L59 v2.0.0, rework implementation as handleSearchParam}
+ * {@link https://github.com/renoirb/archivator/blob/v2.0.0/src/normalizer/slugs.js#L18-L59 | v2.0.0, rework implementation as handleSearchParam}
+ *
+ * @param url - The URL to convert into a valid path name
+ * @param removeAccents - Should we remove any accents before normalizing (default: false)
  */
-export const main = (url: string): string => {
-  const search = searchParams(url)
-  const pathname = pathName(url)
-  const urlObj = toUrl(url)
+export const main = (url: string, removeAccents = false): string => {
+  let nUrl = url
+  if (removeAccents === true) {
+    // Remove accents
+    nUrl = nUrl.normalize('NFD').replace(/[\u0300-\u036F]/g, '')
+  }
+  const search = searchParams(nUrl)
+  const pathname = pathName(nUrl)
+  const urlObj = toUrl(nUrl)
   return String(`${urlObj.hostname}${pathname}${search}`)
     .toLowerCase()
     .replace(/(www\.)/g, '')
--- packages/url-dirname-normalizer/src/normalizer/path-name.ts
+++ packages/url-dirname-normalizer/src/normalizer/path-name.ts
@@ -9,12 +9,14 @@ import { toUrl } from './url'
 export const pathName = (url: string): string => {
   const urlObj = toUrl(url)
   let out = String(urlObj.pathname).toLowerCase()
+  // Remove things that aren't really useful
   out = out.replace(/-[a-z0-9]{5,}$/, '')
   out = out.replace(/%40/, '_at_')
   out = out.replace(/\.(action|fcgi|do)/, '')
+  out = out.replace(/cgi-bin\//, '/')
   out = out.replace(/\/$/, '')
   out = out.replace(/:/, '/')
-  out = out.replace(/\/\//, '/')
+  out = out.replace(/\/\//g, '/')
   out = out.replace(/[@=%&#()~!,]+/g, '')
   out = out.replace(/\.(s?html?|php|xml|aspx?)/, '')
 
